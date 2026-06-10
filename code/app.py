a4ce8f73e169effd273f40661b36c154121746fe
RickyS <35142826+abliskan@users.noreply.github.com>
2026-06-10 15:24:59 +0000

    Update: debug the code file

--- code/app.py
+++ code/app.py
@@ -1,9 +1,16 @@
+import os
 from dotenv import load_dotenv
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 load_dotenv()
 
+# Access your environment variables
+my_var = os.getenv("GOOGLE_API_KEY")
+
 # Load the Gemini API key
-embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001", google_api_key=userdata.get('gemini_api'))
+# embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001", google_api_key=userdata.get('gemini_api'))
 
 # Initialize the Gemini model
-llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash", temperature=0.3
+llm = ChatGoogleGenerativeAI(model="gemini-3.5-flash")
+
+response = llm.invoke("I just discovered the course. Can I join now?")
+print(response)
