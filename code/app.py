e3e534b175603d5bcb79fb026c1cc53a7a869786
RickyS <35142826+abliskan@users.noreply.github.com>
2026-06-12 16:59:53 +0000

    Update: debug the code file

--- code/app.py
+++ code/app.py
@@ -1,11 +1,13 @@
 import os
 import requests
 from dotenv import load_dotenv
-from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
+# from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
+from google import genai
 load_dotenv()
 
 # Access your environment variables
-my_var = os.getenv("GOOGLE_API_KEY")
+# my_var = os.getenv("GOOGLE_API_KEY")
+client = genai.Client(api_key=os.getenv("GOOGLE_API_KEY"))
 
 # Load the Gemini API key
 # embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001", google_api_key=userdata.get('gemini_api'))
@@ -13,26 +15,30 @@ my_var = os.getenv("GOOGLE_API_KEY")
 # Initialize the Gemini model
 # llm = ChatGoogleGenerativeAI(model="gemini-3.5-flash")
 
+response = client.models.generate_content(
+    model="gemini-3.5-flash",
+    contents="I just discovered the course. Can I join now?"
+)
 # response = llm.invoke("I just discovered the course. Can I join now?")
-# print(response)
+print(response.text)
 
-docs_url = 'https://datatalks.club/faq/json/courses.json'
-response = requests.get(docs_url)
-courses_raw = response.json()
+# docs_url = 'https://datatalks.club/faq/json/courses.json'
+# response = requests.get(docs_url)
+# courses_raw = response.json()
 
-# print(courses_raw)
+# # print(courses_raw)
 
-documents = []
-url_prefix = 'https://datatalks.club/faq'
+# documents = []
+# url_prefix = 'https://datatalks.club/faq'
 
-for course in courses_raw:
-    course_url = f'{url_prefix}{course['path']}'
+# for course in courses_raw:
+#     course_url = f'{url_prefix}{course['path']}'
 
-    course_response = requests.get(course_url)
-    course_response.raise_for_status()
-    course_data = course_response.json()
+#     course_response = requests.get(course_url)
+#     course_response.raise_for_status()
+#     course_data = course_response.json()
 
-    documents.extend(course_data)
+#     documents.extend(course_data)
 
-print("Length of documents", len(documents))
-print("Show inside documents", documents[1100])
+# print("Length of documents", len(documents))
+# print("Show inside documents", documents[1100])
