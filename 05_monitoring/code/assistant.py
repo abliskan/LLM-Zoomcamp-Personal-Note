10fd07a34110eeee0bfef971ecc6614fe354b7c5
RickyS <suhanryricky@yahoo.com>
2026-07-21 21:50:36 +0700

    Update: debug the code file wk-5

--- 05_monitoring/code/assistant.py
+++ 05_monitoring/code/assistant.py
@@ -3,8 +3,9 @@ import os
 
 from dotenv import load_dotenv
 from ingest import load_faq_data, build_index
-from rag_helper import RAGBase
+# from rag_helper import RAGBase
 from metrics import RAGWithMetrics
+from db_save import save_conversation
 from google import genai
 client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
@@ -29,3 +30,5 @@ if __name__ == "__main__":
 
     answer = assistant.rag(query)
     print(answer)
+    
+    save_conversation(assistant.last_call, query, "llm-zoomcamp")
